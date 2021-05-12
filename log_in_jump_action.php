8f6b05b115c3f14e679e106b3198b72b39160eec
SongweiLai-1 <48987293+SongweiLai-1@users.noreply.github.com>
2021-05-12 20:39:34 +1000

    Update log_in_jump_action.php

--- log_in_jump_action.php
+++ log_in_jump_action.php
@@ -12,8 +12,6 @@ $dbname = "smart_home";
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
     include ("get_ip_function.php");
-    require ("get_location.php");
-
     $conn = new mysqli($servername, $username, $password, $dbname);
     $cus_email = $_POST['email'];
     $cus_password = $_POST['password'];
@@ -22,21 +20,17 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
         die("CONNECT FAILD, PLEASE TRY AGAGIN LATTER: " . $conn->connect_error);
     }
 
+
     $sql = "SELECT * FROM user_inf WHERE (Email_address = '$cus_email') AND (Cus_password='$cus_password')";
-    $insert_location
+   // $insert_location = "INSERT INTO location_log (cus_id,country_name,city_name,continent_Name,latitude,longitude,time_zone) VALUE ( ".$_SESSION['user_account'].",'$country_name','$Continent_Name','$Latitude' ,'$Longitude','$time_zone')";
+  //  $conn->query($insert_location);
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
     $_SESSION['user_account'] = $row;
     $_SESSION['email']= $_POST['email'];
-    include ('platform_built.php');
+
+
 }
-
-$select_plateform = "SELECT count(*) FROM platform";
-$plateform_result = mysqli_query($conn,$select_plateform);
-$rows = mysqli_fetch_row($plateform_result);
-$rowcount = $rows[0];
-
-
 
 
 ?>
@@ -47,8 +41,7 @@ $rowcount = $rows[0];
 </script>
 
 <?php
-
-
+include ("platform_built.php");
 mysqli_close($conn); ?>
 
 
